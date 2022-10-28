060fa9d5a2e97b73531edf983201b34271852d2b
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-10-28 21:33:55 +0600

    environment add

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -7,14 +7,23 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDg7-V24qNid_bcqtpcmQ37VS7IUxwKRME",
-  authDomain: "assignment-10-authentica-58f9f.firebaseapp.com",
-  projectId: "assignment-10-authentica-58f9f",
-  storageBucket: "assignment-10-authentica-58f9f.appspot.com",
-  messagingSenderId: "492993986590",
-  appId: "1:492993986590:web:d5dd1793360d33837eae0f",
-  measurementId: "G-HPY6RYS5NY"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDg7-V24qNid_bcqtpcmQ37VS7IUxwKRME",
+//   authDomain: "assignment-10-authentica-58f9f.firebaseapp.com",
+//   projectId: "assignment-10-authentica-58f9f",
+//   storageBucket: "assignment-10-authentica-58f9f.appspot.com",
+//   messagingSenderId: "492993986590",
+//   appId: "1:492993986590:web:d5dd1793360d33837eae0f",
+//   measurementId: "G-HPY6RYS5NY"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
