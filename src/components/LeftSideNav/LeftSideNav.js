60a26dc04213304b675c4b617daf7e9bd4ec8c9e
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-10-28 23:42:16 +0600

    link added

--- src/components/LeftSideNav/LeftSideNav.js
+++ src/components/LeftSideNav/LeftSideNav.js
@@ -9,7 +9,7 @@ const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/course-categories")
+    fetch("https://b610-server-site.vercel.app/course-categories")
       .then((res) => res.json())
       .then((data) => setCategories( data))
   }, []);
